640621af6612bf5131738fa86b3e7b584fe01401
John Taylor <jttaylor63@gmail.com>
2026-06-17 10:34:47 -0400

    feat(dnd): addDnd notifier method

--- test/character_provider_test.dart
+++ test/character_provider_test.dart
@@ -35,4 +35,20 @@ void main() {
     expect(chars.first.starforged, isNotNull);
     expect(chars.first.starforged!.edge, 3);
   });
+
+  test('addDnd prepends a premade D&D character', () async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({
+      'juice.sessions.v1':
+          '{"active":"default","sessions":[{"id":"default","name":"C1"}]}',
+    });
+    final c = ProviderContainer();
+    addTearDown(c.dispose);
+    await c.read(charactersProvider.future);
+    final id = await c.read(charactersProvider.notifier).addDnd();
+    final chars = await c.read(charactersProvider.future);
+    expect(chars.first.id, id);
+    expect(chars.first.dnd, isNotNull);
+    expect(chars.first.dnd!.className, 'Fighter');
+  });
 }
